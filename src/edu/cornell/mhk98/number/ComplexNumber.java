4b4a78d525d12582e705f742798353d93eebacbf
muhammadkhan <mhk98@cornell.edu>
2014-06-16 01:17:36 -0400

    added ComplexNumber

--- src/edu/cornell/mhk98/number/ComplexNumber.java
+++ src/edu/cornell/mhk98/number/ComplexNumber.java
@@ -5,4 +5,17 @@ public class ComplexNumber{
     private double re;
     private double im;
 
+    public ComplexNumber(double a, double b){
+	re = a;
+	im = b;
+    }
+
+    public double modulus(){
+	return Math.sqrt(Math.pow(re,2) + Math.pow(im, 2));
+    }
+
+    public double arg(){
+	return Math.atan(im / re);
+    }
+
 }
